a89848b05edffca637a46171d0bc1a6d5fa2b803
Thibault Collet <ton.email@exemple.com>
2025-10-07 16:04:25 +0200

    fix: import DonationMobile via @ alias

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -2,7 +2,7 @@
 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import DonationMobile from "../../../components/DonationMobile";
+import DonationMobile from "@/components/DonationMobile";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
