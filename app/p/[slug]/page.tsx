0f9d06fe7b6a39b76820dc1c812bb3d70cf51ce6
Thibault Collet <ton.email@exemple.com>
2025-10-07 15:56:52 +0200

    fix tsconfig alias + import

--- app/p/[slug]/page.tsx
+++ app/p/[slug]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import DonationMobile from "Quete-app/components/DonationMobile";
+import DonationMobile from "../../../components/DonationMobile";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
